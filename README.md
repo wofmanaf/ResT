494ccf8b077684350808676f75d3cf4b4e27d5fc
wofmanaf <wofmanaf@gmail.com>
2021-05-31 08:35:05 +0800

    source code

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Code for ResT: An Efficient Transformer for Visual Recognition, paper and codes will be available in June.
- 
+# ResT
+By Qing-Long Zhang and Yu-Bin Yang
+
+[State Key Laboratory for Novel Software Technology at Nanjing University]
+
+This repo is the official implementation of ["ResT: An Efficient Transformer for Visual Recognition"](https://arxiv.org/pdf/2105.13677v1.pdf). It currently includes code and models for the following tasks:
+> **Image Classification**: Included in this repo. See [get_started.md](get_started.md) for a quick start.
+
+> **Object Detection and Instance Segmentation**: Based on [detectron2](https://github.com/facebookresearch/detectron2), coming soon.
+
+**ResT** is initially described in [arxiv](https://arxiv.org/pdf/2105.13677v1.pdf), which capably serves as a
+general-purpose backbone for computer vision. It can tackle input images with arbitrary size. Besides, 
+ResT compressed the memory of standard MSA and model the interaction between multi-heads while keeping 
+the diversity ability. 
+
+
+## Main Results on ImageNet with Pretrained Models
+
+**ImageNet-1K Pretrained Models**
+
+| name | resolution |acc@1 | acc@5 | #params | FLOPs | FPS| 1K model |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: |:---: |
+| ResT-Lite  | 224x224 | 77.2 | 93.7 | 10.5M | 1.4G | 1246 | [baidu](https://pan.baidu.com/s/1PSqwEvwfdqSGB4tlL9kFRg)
+| ResT-Small | 224x224 | 79.6 | 94.9 | 13.7M | 1.9G | 1043 | [baidu](https://pan.baidu.com/s/1lx33vDMdPyw4U9sgaKvv4g)
+| ResT-Base  | 224x224 | 81.6 | 95.7 | 30.3M | 4.3G | 673  | [baidu](https://pan.baidu.com/s/1sSi7r83ujb146WhU8F-wGw)
+| ResT-Large | 224x224 | 83.6 | 96.3 | 51.6M | 7.9G | 429 | [baidu](https://pan.baidu.com/s/1lVStrppan4nbAqCEuNvRDg)
+
+
+Note: access code for `baidu` is `rest`.
+
+## Citing ResT
+
+```
+@article{zhql2021ResT,
+  title={ResT: An Efficient Transformer for Visual Recognition},
+  author={Zhang, Qinglong and Yang, Yubin},
+  journal={arXiv preprint arXiv:2105.13677},
+  year={2021}
+}
+```
---
+++ models/rest.py
@@ -0,0 +1,349 @@
+import torch
+import torch.nn as nn
+
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+from timm.models.registry import register_model
+
+__all__ = ['rest_lite', 'rest_small', 'rest_base', 'rest_large']
+
+
+def _cfg(url='', **kwargs):
+    return {
+        'url': url,
+        'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
+        'crop_pct': .9, 'interpolation': 'bicubic',
+        'mean': (0.485, 0.456, 0.406), 'std': (0.229, 0.224, 0.225),
+        'classifier': 'head',
+        **kwargs
+    }
+
+
+default_cfgs = {
+    'rest_lite': _cfg(),
+    'rest_small': _cfg(),
+    'rest_base': _cfg(),
+    'rest_large': _cfg(),
+}
+
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self,
+                 dim,
+                 num_heads=8,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 attn_drop=0.,
+                 proj_drop=0.,
+                 sr_ratio=1,
+                 apply_transform=False):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        self.sr_ratio = sr_ratio
+        if sr_ratio > 1:
+            self.sr = nn.Conv2d(dim, dim, kernel_size=sr_ratio+1, stride=sr_ratio, padding=sr_ratio // 2, groups=dim)
+            self.sr_norm = nn.LayerNorm(dim)
+
+        self.apply_transform = apply_transform and num_heads > 1
+        if self.apply_transform:
+            self.transform_conv = nn.Conv2d(self.num_heads, self.num_heads, kernel_size=1, stride=1)
+            self.transform_norm = nn.InstanceNorm2d(self.num_heads)
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        if self.sr_ratio > 1:
+            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            x_ = self.sr_norm(x_)
+            kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.kv(x).reshape(B, N, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        k, v = kv[0], kv[1]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        if self.apply_transform:
+            attn = self.transform_conv(attn)
+            attn = attn.softmax(dim=-1)
+            attn = self.transform_norm(attn)
+        else:
+            attn = attn.softmax(dim=-1)
+
+        attn = self.attn_drop(attn)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, sr_ratio=1, apply_transform=False):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=drop, sr_ratio=sr_ratio, apply_transform=apply_transform)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x, H, W):
+        x = x + self.drop_path(self.attn(self.norm1(x), H, W))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+
+class PA(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.pa_conv = nn.Conv2d(dim, dim, kernel_size=3, padding=1, groups=dim)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return x * self.sigmoid(self.pa_conv(x))
+
+
+class GL(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.gl_conv = nn.Conv2d(dim, dim, kernel_size=3, padding=1, groups=dim)
+
+    def forward(self, x):
+        return x + self.gl_conv(x)
+
+
+class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding"""
+    def __init__(self, patch_size=16, in_ch=3, out_ch=768, with_pos=False):
+        super().__init__()
+        self.patch_size = to_2tuple(patch_size)
+        self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=patch_size+1, stride=patch_size, padding=patch_size // 2)
+        self.norm = nn.BatchNorm2d(out_ch)
+
+        self.with_pos = with_pos
+        if self.with_pos:
+            self.pos = PA(out_ch)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        x = self.conv(x)
+        x = self.norm(x)
+        if self.with_pos:
+            x = self.pos(x)
+        x = x.flatten(2).transpose(1, 2)
+        H, W = H // self.patch_size[0], W // self.patch_size[1]
+        return x, (H, W)
+
+
+class BasicStem(nn.Module):
+    def __init__(self, in_ch=3, out_ch=64, with_pos=False):
+        super(BasicStem, self).__init__()
+        hidden_ch = out_ch // 2
+        self.conv1 = nn.Conv2d(in_ch, hidden_ch, kernel_size=3, stride=2, padding=1, bias=False)
+        self.norm1 = nn.BatchNorm2d(hidden_ch)
+        self.conv2 = nn.Conv2d(hidden_ch, hidden_ch, kernel_size=3, stride=1, padding=1, bias=False)
+        self.norm2 = nn.BatchNorm2d(hidden_ch)
+        self.conv3 = nn.Conv2d(hidden_ch, out_ch, kernel_size=3, stride=2, padding=1, bias=False)
+
+        self.act = nn.ReLU(inplace=True)
+        self.with_pos = with_pos
+        if self.with_pos:
+            self.pos = PA(out_ch)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.act(x)
+
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.act(x)
+
+        x = self.conv3(x)
+        if self.with_pos:
+            x = self.pos(x)
+        return x
+
+
+class Stem(nn.Module):
+    def __init__(self, in_ch=3, out_ch=64, with_pos=False):
+        super(Stem, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=7, stride=2, padding=3, bias=False)
+        self.norm = nn.BatchNorm2d(out_ch)
+        self.act = nn.ReLU(inplace=True)
+
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.with_pos = with_pos
+        if self.with_pos:
+            self.pos = PA(out_ch)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.act(x)
+        x = self.max_pool(x)
+
+        if self.with_pos:
+            x = self.pos(x)
+        return x
+
+
+class ResT(nn.Module):
+    def __init__(self, in_chans=3, num_classes=1000, embed_dims=[64, 128, 256, 512],
+                 num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=False,
+                 qk_scale=None, drop_rate=0., attn_drop_rate=0., drop_path_rate=0.,
+                 depths=[2, 2, 2, 2], sr_ratios=[8, 4, 2, 1],
+                 norm_layer=nn.LayerNorm, apply_transform=False):
+        super().__init__()
+        self.num_classes = num_classes
+        self.depths = depths
+        self.apply_transform = apply_transform
+
+        self.stem = BasicStem(in_ch=in_chans, out_ch=embed_dims[0], with_pos=True)
+
+        self.patch_embed_2 = PatchEmbed(patch_size=2, in_ch=embed_dims[0], out_ch=embed_dims[1], with_pos=True)
+        self.patch_embed_3 = PatchEmbed(patch_size=2, in_ch=embed_dims[1], out_ch=embed_dims[2], with_pos=True)
+        self.patch_embed_4 = PatchEmbed(patch_size=2, in_ch=embed_dims[2], out_ch=embed_dims[3], with_pos=True)
+
+        # transformer encoder
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+        cur = 0
+
+        self.stage1 = nn.ModuleList([
+            Block(embed_dims[0], num_heads[0], mlp_ratios[0], qkv_bias, qk_scale, drop_rate, attn_drop_rate,
+                  drop_path=dpr[cur+i], norm_layer=norm_layer, sr_ratio=sr_ratios[0], apply_transform=apply_transform)
+            for i in range(self.depths[0])])
+
+        cur += depths[0]
+        self.stage2 = nn.ModuleList([
+            Block(embed_dims[1], num_heads[1], mlp_ratios[1], qkv_bias, qk_scale, drop_rate, attn_drop_rate,
+                  drop_path=dpr[cur+i], norm_layer=norm_layer, sr_ratio=sr_ratios[1], apply_transform=apply_transform)
+            for i in range(self.depths[1])])
+
+        cur += depths[1]
+        self.stage3 = nn.ModuleList([
+            Block(embed_dims[2], num_heads[2], mlp_ratios[2], qkv_bias, qk_scale, drop_rate, attn_drop_rate,
+                  drop_path=dpr[cur+i], norm_layer=norm_layer, sr_ratio=sr_ratios[2], apply_transform=apply_transform)
+            for i in range(self.depths[2])])
+
+        cur += depths[2]
+        self.stage4 = nn.ModuleList([
+            Block(embed_dims[3], num_heads[3], mlp_ratios[3], qkv_bias, qk_scale, drop_rate, attn_drop_rate,
+                  drop_path=dpr[cur+i], norm_layer=norm_layer, sr_ratio=sr_ratios[3], apply_transform=apply_transform)
+            for i in range(self.depths[3])])
+
+        self.norm = norm_layer(embed_dims[3])
+
+        # classification head
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.head = nn.Linear(embed_dims[3], num_classes) if num_classes > 0 else nn.Identity()
+
+        # init weights
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            trunc_normal_(m.weight, std=0.02)
+        elif isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=0.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, (nn.LayerNorm, nn.BatchNorm2d)):
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.stem(x)
+        B, _, H, W = x.shape
+        x = x.flatten(2).permute(0, 2, 1)
+
+        # stage 1
+        for blk in self.stage1:
+            x = blk(x, H, W)
+        x = x.permute(0, 2, 1).reshape(B, -1, H, W)
+
+        # stage 2
+        x, (H, W) = self.patch_embed_2(x)
+        for blk in self.stage2:
+            x = blk(x, H, W)
+        x = x.permute(0, 2, 1).reshape(B, -1, H, W)
+
+        # stage 3
+        x, (H, W) = self.patch_embed_3(x)
+        for blk in self.stage3:
+            x = blk(x, H, W)
+        x = x.permute(0, 2, 1).reshape(B, -1, H, W)
+
+        # stage 4
+        x, (H, W) = self.patch_embed_4(x)
+        for blk in self.stage4:
+            x = blk(x, H, W)
+        x = self.norm(x)
+
+        x = x.permute(0, 2, 1).reshape(B, -1, H, W)
+        x = self.avg_pool(x).flatten(1)
+        x = self.head(x)
+        return x
+
+
+@register_model
+def rest_lite(pretrained=False, **kwargs):
+    model = ResT(embed_dims=[64, 128, 256, 512], num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=True,
+                 depths=[2, 2, 2, 2], sr_ratios=[8, 4, 2, 1], apply_transform=True, **kwargs)
+    model.default_cfg = _cfg()
+    return model
+
+
+@register_model
+def rest_small(pretrained=False, **kwargs):
+    model = ResT(embed_dims=[64, 128, 256, 512], num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=True,
+                 depths=[2, 2, 6, 2], sr_ratios=[8, 4, 2, 1], apply_transform=True, **kwargs)
+    model.default_cfg = _cfg()
+    return model
+
+
+@register_model
+def rest_base(pretrained=False, **kwargs):
+    model = ResT(embed_dims=[96, 192, 384, 768], num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=True,
+                 depths=[2, 2, 6, 2], sr_ratios=[8, 4, 2, 1], apply_transform=True, **kwargs)
+    model.default_cfg = _cfg()
+    return model
+
+
+@register_model
+def rest_large(pretrained=False, **kwargs):
+    model = ResT(embed_dims=[96, 192, 384, 768], num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=True,
+                 depths=[2, 2, 18, 2], sr_ratios=[8, 4, 2, 1], apply_transform=True, **kwargs)
+    model.default_cfg = _cfg()
+    return model
+
